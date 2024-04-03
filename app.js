241e0366d548d396f681c4127232fb4c06cbda3e
Gourish <naga.g.bhimanapati@chase.com>
2024-04-03 14:50:47 +0530

    forward button event handle

--- app.js
+++ app.js
@@ -1,50 +1,84 @@
-
-
-const myAudioFile = document.querySelector("audio")
-console.log(myAudioFile)
-
-
-const myplayButton = document.querySelector("#play")
-var shouldPlay = true
-
-myplayButton.addEventListener("click", function() {
-    if (shouldPlay === true) {
-        myAudioFile.play()
-        myplayButton.classList.replace("fa-play", "fa-pause")
-    } else {
-        myAudioFile.pause()
-        myplayButton.classList.replace("fa-pause", "fa-play")
-    }
-    shouldPlay = !shouldPlay
-})
-
-
-const myForwardButton = document.querySelector("#play")
-
-const myCurrentTimeLabel = document.querySelector(".currenttime")
-const myTotalTimeLabel = document.querySelector(".totaltime")
-
-
-myAudioFile.addEventListener("timeupdate", function(event) {
-
-    let durationInMin = Math.floor(event.srcElement.duration / 60)
-    let durationInSec = Math.floor(event.srcElement.duration % 60)
-    myTotalTimeLabel.textContent = `${durationInMin}:${durationInSec}`
-    
-
-    let currentTimeInMin = Math.floor(event.srcElement.currentTime / 60)
-    let currentTimeInSec = Math.floor(event.srcElement.currentTime % 60)
-    if(currentTimeInSec < 10) {
-        currentTimeInSec = `0${currentTimeInSec}`
-    }
-    myCurrentTimeLabel.textContent = `${currentTimeInMin}:${currentTimeInSec}`
-})
-
 const songsData = [
     {
         songName: "smooth criminal",
         Album: "MJ",
         audioFile: "audio1.mp3",
         imageFile: "image1.jpg"
+    },
+    {
+        songName: "black or white",
+        Album: "MJ",
+        audioFile: "audio1.mp3",
+        imageFile: "image2.jpg"
+    },
+    {
+        songName: "jam",
+        Album: "MJ",
+        audioFile: "audio1.mp3",
+        imageFile: "image3.jpg"
     }
 ]
+
+const myAudioFile = document.querySelector("audio")
+const myplayButton = document.querySelector("#play")
+const myForwardButton = document.querySelector("#forward")
+const myCurrentTimeLabel = document.querySelector(".currenttime")
+const myTotalTimeLabel = document.querySelector(".totaltime")
+const songNameLabel = document.querySelector("#songname")
+const songAlbumLabel = document.querySelector("#songalbum")
+const songImage = document.querySelector("#songimage")
+
+var shouldPlay = true
+var currentSongIndex = -1
+
+function playAudio() {
+    myAudioFile.play()
+    myplayButton.classList.replace("fa-play", "fa-pause")
+}
+
+function pauseAudio() {
+    myAudioFile.pause()
+    myplayButton.classList.replace("fa-pause", "fa-play")
+}
+
+function LoadSong(songInfo) {
+    console.log(songInfo)
+
+    if(currentSongIndex >= songsData.length - 1) {
+        currentSongIndex = -1;
+    }
+    songNameLabel.textContent = songInfo.songName
+    songAlbumLabel.textContent = songInfo.Album
+    songImage.src = `images/${songInfo.imageFile}`
+    myAudioFile.src = `audios/${songInfo.audioFile}`
+    playAudio()
+}
+
+myplayButton.addEventListener("click", function () {
+    if (shouldPlay === true) {
+        playAudio()
+    } else {
+        pauseAudio()
+    }
+    shouldPlay = !shouldPlay
+})
+
+myAudioFile.addEventListener("timeupdate", function (event) {
+
+    let durationInMin = Math.floor(event.srcElement.duration / 60)
+    let durationInSec = Math.floor(event.srcElement.duration % 60)
+    myTotalTimeLabel.textContent = `${durationInMin}:${durationInSec}`
+
+
+    let currentTimeInMin = Math.floor(event.srcElement.currentTime / 60)
+    let currentTimeInSec = Math.floor(event.srcElement.currentTime % 60)
+    if (currentTimeInSec < 10) {
+        currentTimeInSec = `0${currentTimeInSec}`
+    }
+    myCurrentTimeLabel.textContent = `${currentTimeInMin}:${currentTimeInSec}`
+})
+
+myForwardButton.addEventListener("click", function () {
+    currentSongIndex++;
+    LoadSong(songsData[currentSongIndex])
+})
---
+++ data/data.js
@@ -0,0 +1,20 @@
+const songsData = [
+    {
+        songName: "smooth criminal",
+        Album: "MJ",
+        audioFile: "audio1.mp3",
+        imageFile: "image1.jpg"
+    },
+    {
+        songName: "black or white",
+        Album: "MJ",
+        audioFile: "audio1.mp3",
+        imageFile: "image2.jpg"
+    },
+    {
+        songName: "jam",
+        Album: "MJ",
+        audioFile: "audio1.mp3",
+        imageFile: "image3.jpg"
+    }
+]
